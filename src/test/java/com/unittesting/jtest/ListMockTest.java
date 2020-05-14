4b06cd4e601e9f1a9d55ba5cbb60a601aadbc819
Msameh93 <msameh.93@hotmail.com>
2020-05-14 00:34:19 -0700

    Mockito unit tests

--- src/test/java/com/unittesting/jtest/ListMockTest.java
+++ src/test/java/com/unittesting/jtest/ListMockTest.java
@@ -1,16 +1,22 @@
 package com.unittesting.jtest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.anyInt;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 class ListMockTest {
-	List mockList= mock(List.class);
+	List<String> mockList= mock(List.class);
 
 	@Test
 	void sizeBasic() {
@@ -33,11 +39,36 @@ class ListMockTest {
 		assertThat(mockList.get(0)).isEqualTo("Hello");
 	}
 	@Test
-	void genericParameters() {
+	void argumentMatchers() {
 		when(mockList.get(anyInt())).thenReturn("Hello");
 		
 		assertThat(mockList.get(0)).isEqualTo("Hello");
 		assertThat(mockList.get(1)).isEqualTo("Hello");
 		assertThat(mockList.get(2)).isEqualTo("Hello");
+	}
+	@Test
+	void methodCallVerification() {
+		mockList.get(0);
+		mockList.get(1);
+		
+		//verifies method gets called
+		verify(mockList).get(anyInt());
+		verify(mockList, times(2)).get(anyInt()); //if called 1 time - DEFAULT is 1
+		verify(mockList, atLeast(1)/*atLeastOnce()*/).get(anyInt()); 
+		verify(mockList, atMost(2)).get(anyInt()); 
+		verify(mockList, never()).get(2); 		//never gets called with value 2
+
+
+	}
+	@Test
+	void captureArgument() {
+		mockList.add("Hello");
+		
+		//capture object that captures the argument
+		ArgumentCaptor<String> captor= ArgumentCaptor.forClass(String.class);
+	
+		verify(mockList).add(captor.capture());
+		
+		assertThat("Hello").isEqualTo(captor.getValue());
 	}
 }
